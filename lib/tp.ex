d0ae1ad8986022b8e8c47c5a9388bc21ce6246a0
Javier Acero <j4cegu@gmail.com>
2017-05-20 10:28:58 +0200

    TP can run a single function (F)

--- lib/tp.ex
+++ lib/tp.ex
@@ -1,7 +1,5 @@
 defmodule Tp do
 
-  def run(fun),        do: fun.()
-  def run(fun, params) when is_list(params), do: apply(fun, params)
-  def run(fun, param), do: fun.(param)
+  def run(fun, params), do: apply(fun, params)
 
 end
