b8e083791af8deeddaea96875a5bb8b191cccafa
Javier Acero <j4cegu@gmail.com>
2017-05-20 10:28:25 +0200

    TP can run a single function (F)

--- test/tp_test.exs
+++ test/tp_test.exs
@@ -1,16 +1,9 @@
 defmodule TpTest do
   use ExUnit.Case
 
-  test "runs an anonymous function without params" do
-    assert Tp.run(fn -> 2 + 2 end) == 4
-    assert Tp.run(fn -> String.upcase("foo") end) == "FOO"
-  end
-
-  test "runs an anonymous function with 1 param" do
-    assert Tp.run(fn(x) -> 2 + x end, 2) == 4
-  end
-
-  test "runs an anonymous function with 2 params" do
+  test "runs a single function" do
+    assert Tp.run(fn -> 2 + 2 end, []) == 4
+    assert Tp.run(&String.upcase/1, ["foo"]) == "FOO"
     assert Tp.run(fn(x, y) -> x + y end, [1, 2]) == 3
   end
 
