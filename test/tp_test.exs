6b30c26204170f41731904497858c97ae80f0c60
Javier Acero <j4cegu@gmail.com>
2017-05-20 15:50:36 +0200

    TP can run multiple functions (F)
    Since we already have a test case for a single function the previous
    tests are redundant.
    
    Note that we could keep them and simply wrap the function in a list
    (so we provide a unified interface that always takes a list of functions
    and a list of parameters for the pipeline).

--- test/tp_test.exs
+++ test/tp_test.exs
@@ -1,12 +1,6 @@
 defmodule TpTest do
   use ExUnit.Case
 
-  test "runs a single function" do
-    assert Tp.run(fn -> 2 + 2 end, []) == 4
-    assert Tp.run(&String.upcase/1, ["foo"]) == "FOO"
-    assert Tp.run(fn(x, y) -> x + y end, [1, 2]) == 3
-  end
-
   test "runs a list of functions in order" do
     fun1 = fn(x, y, z) -> [x*x, y*y, z*z] end
     fun2 = fn(x, y, z) -> [z, y, x] end
