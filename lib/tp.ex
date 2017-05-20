43bcb157ef1710cc83b9d0fa7e81e728f69fd429
Javier Acero <j4cegu@gmail.com>
2017-05-20 08:28:21 +0200

    Initial commit

---
+++ lib/tp.ex
@@ -0,0 +1,18 @@
+defmodule Tp do
+  @moduledoc """
+  Documentation for Tp.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Tp.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/tp_test.exs
@@ -0,0 +1,8 @@
+defmodule TpTest do
+  use ExUnit.Case
+  doctest Tp
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
