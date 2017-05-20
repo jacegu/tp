22b5834f3e8af789837f02dd65934ce0a17fb8e6
Javier Acero <j4cegu@gmail.com>
2017-05-20 15:53:58 +0200

    TP can run multiple functions (F)
    Simplify the interface of the function so we always pass in a list of
    functions and a list of arguments.

--- lib/tp.ex
+++ lib/tp.ex
@@ -1,10 +1,9 @@
 defmodule Tp do
 
-  def run(funs, params) when is_list(funs) do
+  def run(funs, params) do
     Enum.reduce(funs, params, fn(fun, result) ->
       apply(fun, result)
     end)
   end
-  def run(fun, params), do: apply(fun, params)
 
 end
