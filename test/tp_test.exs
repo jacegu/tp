ddf9bc89305a412f3951f9b4953bbff163f6ac2c
Javier Acero <j4cegu@gmail.com>
2017-05-20 08:57:31 +0200

    TP can run an anonymous function 2 (R)

--- test/tp_test.exs
+++ test/tp_test.exs
@@ -3,6 +3,7 @@ defmodule TpTest do
 
   test "runs an anonymous function without params" do
     assert Tp.run(fn -> 2 + 2 end) == 4
+    assert Tp.run(fn -> String.upcase("foo") end) == "FOO"
   end
 
 end
