851a1b0ffb1dc9977c2b8c626d9673a5e49023e8
Javier Acero <j4cegu@gmail.com>
2017-05-20 15:49:41 +0200

    TP can run multiple functions (F)
    We add two new test cases:
    - Empty list of function
    - List of functions with only one function

--- test/tp_test.exs
+++ test/tp_test.exs
@@ -7,9 +7,12 @@ defmodule TpTest do
     assert Tp.run(fn(x, y) -> x + y end, [1, 2]) == 3
   end
 
-  test "runs multiple functions" do
+  test "runs a list of functions in order" do
     fun1 = fn(x, y, z) -> [x*x, y*y, z*z] end
     fun2 = fn(x, y, z) -> [z, y, x] end
+
+    assert Tp.run([], [1, 2, 3]) == [1, 2, 3]
+    assert Tp.run([fun2], [:a, :b, :c]) == [:c, :b, :a]
     assert Tp.run([fun1, fun2], [2, 3, 5]) == [25, 9, 4]
   end
 
